b0a6572760b25b8a38aa9a9e9eec81b909a758c9
cecy <cecilia1995andrade@gmail.com>
2017-10-26 16:59:52 -0500

    Tarea 1 y 2 Curso Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Cecilia';
+title = 'Cecilia';
 colores =['negro','amarillo','verde'];
+ShowColors=  false;
+status= 'Mostrar';
+
+DesplegarArray(){
+this.ShowColors=!this.ShowColors;
+if(this.status=='Ocultar'){
+  this.status='Mostrar';
+}
+else{
+  this.status='Ocultar';
+}
+
+}
+ AddColor(Color){
+   this.colores.push(Color.value);
+   Color.value='';
+   return false;
+ }
+
 }
